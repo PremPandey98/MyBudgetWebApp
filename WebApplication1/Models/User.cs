f45aca9c4bc004eedb27a36aa6ecbf0c10eb189f
Akshay Thakare <akshay.thakare@mtccrm.com>
2025-05-28 15:45:05 +0530

    test changes

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication1.Models;
+
+namespace BudgetMobApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public bool RememberMe { get; set; }
+        public ICollection<BudgetDeposit> BudgetDeposits { get; set; }
+    }
+}
